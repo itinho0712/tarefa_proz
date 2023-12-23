f29bb8051be14519a2058f1e4f6f9228941a3698
Italo <italo.filho@aluno.ufop.edu.br>
2023-12-23 17:42:00 -0300

    first commit

---
+++ tarefa_proz.py
@@ -0,0 +1,11 @@
+# Usando loop for
+for andar in range(20, 0, -1):
+    if andar != 13:
+        print(andar)
+
+# Usando loop while
+andar = 20
+while andar >= 1:
+    if andar != 13:
+        print(andar)
+    andar -= 1
